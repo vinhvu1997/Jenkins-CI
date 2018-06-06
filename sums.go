b7d7c96d54d87ac0f0d4dde803fe6f1923f5dd57
Vinh Vu <vinh@Vinhs-MacBook-Pro.local>
2018-06-06 12:15:58 -0400

    initial commit

---
+++ sums.go
@@ -0,0 +1,11 @@
+package main
+
+
+
+func Sum(x int, y int) int {
+    return x + y
+}
+
+func main() {
+
+}
---
+++ sums_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+    total := Sum(5, 5)
+    if total != 10 {
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+    }
+    total = Sum(8,8)
+    if total != 16 {
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 16)
+    }
+    total = Sum(0,0)
+    if total != 0 {
+	t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 0)
+    }
+}
